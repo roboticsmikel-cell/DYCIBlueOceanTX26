45484832ec8375bf18f905450c7a419cfa6374ca
unknown <mikeldejesus05@gmail.com>
2026-04-24 15:30:39 +0800

    image

--- webServer/src/components/Canvas3D.jsx
+++ webServer/src/components/Canvas3D.jsx
@@ -8,7 +8,7 @@ import DetectionTable from "./DetectionTable";
 import SpeakingAssistantPanel from "./SpeakingAssistantPanel";
 
 const API_URL = import.meta.env.VITE_API_URL;
-const FALLBACK_MODEL_PATH = "/models/vase.glb";
+const FALLBACK_MODEL_PATH = "/models/artifact.glb";
 
 const ARTIFACT_IMAGE_MAP = {
   "vase.glb": "/images/vase.jpg",
