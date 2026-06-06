1742d1ad63967c91114ee7c2573e8aa45e8e7497
unknown <mikeldejesus05@gmail.com>
2026-06-06 10:50:00 +0800

    alab saturday

--- webServer/src/components/StreamPage.jsx
+++ webServer/src/components/StreamPage.jsx
@@ -6,7 +6,8 @@ export default function StreamPage({ onBack }) {
         // src="http://192.168.1.110:5000" // AIR ROAM
         // src="http://192.168.1.168:5000/" // DYCI STARLINK
         // src="http://10.231.46.47:5000/" // MIKEL-1
-        src="http://10.217.166.47:5000/website" // MIKEL-2
+        // src="http://10.217.166.47:5000/website" // MIKEL-2
+        src="http://10.165.142.47:5000/website" // MIKEL-3
         title="AI Vision Stream"
         className="absolute inset-0 h-full w-full"
       />
